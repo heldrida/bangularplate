2aca7a30769b424dcfb3008a4fa6dfd432496716
Helder <heldrida@gmail.com>
2016-06-19 18:58:35 +0100

    Created notes; Changed the boilerplate name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Bangularplate
+# Bang!ularplate
 
 A boilerplate to kickstart AngularJS V1 development using Webpack, Gulp and testing.
 
--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 var angular = require('angular');
 var ngMock = require('angular-mocks');
+var uiRouter = require('angular-ui-router');
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [uiRouter]);
 
 myApp.controller('helloWorldController', ['$scope', function($scope) {
 
