2530f342bb3a1f5bb75d1f57fd76870ebd9acba8
Helder <heldrida@gmail.com>
2016-06-22 01:34:26 +0100

    Amend the preprocessor and files to load, from a requirejs prespective or single entry

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './app/**/*.js',
+      './app/app.js',
       './tests/**/*.js'
     ],
 
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'app/**/*.js': ['webpack'],
+        'app/app.js': ['webpack'],
         'tests/**/*.js': ['webpack']
     },
 
